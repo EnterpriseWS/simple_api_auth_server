1ed19d0bfb1f2a6e8918c24c3ab1580d5ee920e7
Robert Wang <robert.wang@enterprisews.com>
2020-12-31 20:08:52 -0800

    Changed main folder name

--- OAuth2AuthServer/controller.py
+++ OAuth2AuthServer/controller.py
@@ -2,7 +2,7 @@ import cherrypy
 import helper
 
 
-class OauthAuthServer(object):
+class OAuth2AuthServer(object):
     @cherrypy.expose
     def index(self):
         return cherrypy.request.app.config['/']['greeting']
@@ -10,4 +10,4 @@ class OauthAuthServer(object):
 
 if __name__ == '__main__':
     settings = helper.ConfigurationSettingsLocal(source='settings.json').load_all_settings()
-    cherrypy.quickstart(OauthAuthServer(), '/', settings['cherryPy'])
+    cherrypy.quickstart(OAuth2AuthServer(), '/', settings['cherryPy'])
