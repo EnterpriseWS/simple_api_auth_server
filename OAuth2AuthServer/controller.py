08b1b3b9ede0f1c8e197901029430da9ca9bfd39
Robert Wang <robert.wang@enterprisews.com>
2021-01-01 16:32:53 -0800

    Update controller.py

--- OAuth2AuthServer/controller.py
+++ OAuth2AuthServer/controller.py
@@ -1,13 +1,24 @@
-import cherrypy
+import json
+from flask import Flask, jsonify, request
 import helper
 
+_app = Flask(__name__)
+_helper = helper.ConfigurationSettingsLocal(source='settings.json')
 
-class OAuth2AuthServer(object):
-    @cherrypy.expose
-    def index(self):
-        return cherrypy.request.app.config['/']['greeting']
+
+@_app.route('/auth', methods=['GET'])
+def get_auth():
+    grant_type = request.args.get('grant_type')
+    if grant_type == 'client_credentials':
+        return jsonify({'grant_type': 'Got it'})
+
+
+@_app.route('/auth', methods=['POST'])
+def post_auth():
+    grant_type = (json.loads(request.data))['grant_type']
+    if grant_type == 'client_credentials':
+        return jsonify({'grant_type': 'Got it2'})
 
 
 if __name__ == '__main__':
-    settings = helper.ConfigurationSettingsLocal(source='settings.json').load_all_settings()
-    cherrypy.quickstart(OAuth2AuthServer(), '/', settings['cherryPy'])
+    _app.run(debug=True)
